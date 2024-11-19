ef5124135bad00b8adc246ad275fb278c52bea11
nikohonu <nikohonu@proton.me>
2024-11-20 01:57:07 +0200

    it's terrible, but works

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,16 @@
 const std = @import("std");
+const expect = std.testing.expect;
 
 const print = std.debug.print;
+
+fn is_string_in_array(array: std.ArrayList([]u8), string: []const u8) bool {
+    for (array.items) |item| {
+        if (std.mem.eql(u8, item, string)) {
+            return true;
+        }
+    }
+    return false;
+}
 
 const Entry = struct {
     path: []const u8,
@@ -8,8 +18,9 @@ const Entry = struct {
     count: i32,
     children: std.ArrayList(*Entry),
     parent: ?*Entry,
+    allocator: std.mem.Allocator,
 
-    pub fn init(allocator: std.mem.Allocator, path: []const u8, kind: std.fs.File.Kind, parent: ?*Entry) !*Entry {
+    pub fn init(allocator: std.mem.Allocator, path: []const u8, kind: std.fs.File.Kind, parent: ?*Entry, ignore_list: std.ArrayList([]u8)) !*Entry {
         var self = try allocator.create(Entry);
 
         self.* = Entry{
@@ -18,6 +29,7 @@ const Entry = struct {
             .count = 1,
             .children = std.ArrayList(*Entry).init(allocator),
             .parent = parent,
+            .allocator = allocator,
         };
 
         if (self.kind != std.fs.File.Kind.directory) {
@@ -29,13 +41,17 @@ const Entry = struct {
 
         var iter = dir.iterate();
         while (try iter.next()) |item| {
-            // print("{}\n", .{&self});
-            const child = try Entry.init(allocator, try std.fs.path.join(allocator, &[_][]const u8{ self.path, item.name }), item.kind, self);
+            // _ = ignore_list;
+            const child_path = try std.fs.path.join(allocator, &[_][]const u8{ self.path, item.name });
+            if (is_string_in_array(ignore_list, child_path)) {
+                std.debug.print("Skip {s}\n", .{child_path});
+                allocator.free(child_path);
+                continue;
+            }
+            const child = try Entry.init(allocator, child_path, item.kind, self, ignore_list);
             try self.children.append(child);
             self.count += child.count;
         }
-
-        // print("{}\n", .{self});
 
         std.mem.sort(*Entry, self.children.items, {}, cmp);
 
@@ -46,12 +62,13 @@ const Entry = struct {
         return self.count > other.count;
     }
 
-    pub fn deinit(self: *Entry, allocator: std.mem.Allocator) void {
+    pub fn deinit(self: *Entry) void {
         for (self.children.items) |item| {
-            item.deinit(allocator);
+            item.deinit();
         }
         self.children.deinit();
-        allocator.destroy(self);
+        self.allocator.free(self.path);
+        self.allocator.destroy(self);
     }
 
     pub fn format(
@@ -67,20 +84,31 @@ const Entry = struct {
 pub fn main() !void {
     const stdout = std.io.getStdOut();
     const stdin = std.io.getStdIn();
-    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-    defer arena.deinit();
-    const allocator = arena.allocator();
+    // var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    // defer arena.deinit();
+    defer {
+        const deinit_status = gpa.deinit();
+        // _ = gpa.deinit();
+        //fail test; can't try in defer as defer is executed after we return
+        if (deinit_status == .leak) expect(false) catch @panic("TEST FAIL");
+    }
+    // const allocator = arena.allocator();
+    const allocator = gpa.allocator();
 
     const home_path = std.posix.getenv("HOME").?;
 
-    var structure = try Entry.init(allocator, home_path, std.fs.File.Kind.directory, null);
-    defer structure.deinit(allocator);
-    var child: *Entry = structure;
-    // try stdout.writer().print("{}\n", .{child});
-    // child = child.children.items[0];
-    // try stdout.writer().print("{}\n", .{child});
-    // child = child.children.items[0];
-    // try stdout.writer().print("{}\n", .{child});
+    var ignore_list = try IgnoreList.init(allocator, home_path);
+    defer ignore_list.deinit();
+    const flat_ignore_list = try ignore_list.get_flat_ignore_list(allocator);
+    defer flat_ignore_list.deinit();
+
+    // I need to copy home_path to the heap because other paths are stored
+    // there, and I can't free them if one of them is not on the heap.
+    var root = try Entry.init(allocator, try allocator.dupe(u8, home_path), std.fs.File.Kind.directory, null, flat_ignore_list);
+    defer root.deinit();
+    var child: *Entry = root;
+    //
     while (true) {
         if (child.count > 1) {
             child = child.children.items[0];
@@ -89,42 +117,167 @@ pub fn main() !void {
             break;
         }
     }
+    // defer allocator.free(category);
     while (true) {
-        try stdout.writer().writeAll("What to do. Move up (u), Add (a): ");
-        // var buffer: [100]u8 = undefined;
+        try stdout.writer().print("What to do. Move up (u), Add (a), Skip(s): ", .{});
         const input = try stdin.reader().readByte();
+        while (try stdin.reader().readByte() != '\n') {}
         switch (input) {
             'a' => {
                 try stdout.writer().print("Add {s} to ignore list\n", .{child.path});
+                var buf: [128]u8 = undefined;
+                try stdout.writer().print("Enter category of ignored path: ", .{});
+                const user_input = try stdin.reader().readUntilDelimiterOrEof(&buf, '\n');
+                const category = try allocator.dupe(u8, user_input.?);
+                const existing_category = ignore_list.is_category_exist(category);
+                const child_path = try allocator.dupe(u8, child.path);
+                if (existing_category != null) {
+                    try ignore_list.add(existing_category.?, child_path);
+                    allocator.free(category);
+                } else {
+                    try ignore_list.add(category, child_path);
+                }
+                try ignore_list.commit_changes();
+                // try stdin.writer().print("|{s}|", .{category.?});
                 break;
             },
             'u' => {
-                // try stdout.writer().print("Move up {s}\n", .{child.path});
-                child = child.parent orelse continue;
+                child = child.parent orelse root;
                 try stdout.writer().print("Move up {s}\n", .{child});
+            },
+            's' => {
+                break;
             },
             else => {},
         }
     }
-    // try stdout.writer().writeByte(a);
-    // for (structure.children.items) |item| {
-    //     print("{}\n", .{item});
-    // }
-    // print("{any}\n", .{structure});
-
-    // defer home_dir.close();
-    // var iter = home_dir.iterate();
-    //
-    // while (try iter.next()) |item| {
-    //     // var new_entry = Entry{ .path = undefined, .kind = entry.kind };
-    //     try structure.append(Entry.init(allocator, try std.fs.path.join(allocator, &[_][]const u8{ home_path, item.name }), item.kind));
-    //
-    //     // print("{s} {}\n", .{ entry.name, entry.kind });
-    // }
-    //
-    // print("{any}\n", .{structure.items});
-    //
-    // for (structure.items) |item| {
-    //     item.deinit();
-    // }
+    // const x = Result{ .path = "a" };
+    // var map = std.ArrayHashMap([]u8, std.ArrayList([]u8), , false).init(allocator);
+    // map.put("kde", .{ "test1", "test2" });
+    // std.json.
+    // std.json.stringify(value: anytype, options: StringifyOptions, out_stream: anytype)
+    // const map = std.StringHashMap([]u8).init(allocator);
+    // defer map.deinit();
+    // map.put("a", "b");
+    // map.ctx
+    // try ignore_list.add("kde", "fuck");
+    // try ignore_list.add("kde", "test");
+    // try ignore_list.add("xorg", "test");
+    // try ignore_list.print(stdout.writer());
 }
+
+const IgnoreList = struct {
+    data: std.StringHashMap(std.ArrayList([]u8)),
+    allocator: std.mem.Allocator,
+    home_path: []const u8,
+
+    fn init(allocator: std.mem.Allocator, home_path: []const u8) !IgnoreList {
+        const data = std.StringHashMap(std.ArrayList([]u8)).init(allocator);
+        var self: IgnoreList = .{ .data = data, .allocator = allocator, .home_path = home_path };
+        const content = try std.fs.cwd().readFileAlloc(allocator, "ignore.json", 10000);
+        defer allocator.free(content);
+
+        const parsed = try std.json.parseFromSlice(std.json.Value, allocator, content, .{});
+        defer parsed.deinit();
+
+        var iterator = parsed.value.object.iterator();
+        while (iterator.next()) |entry| {
+            // std.debug.print("{s}:\n", .{entry.key_ptr.*});
+            // std.debug.print("{}", .{@TypeOf(entry.value_ptr.array)});
+            const category = try std.fmt.allocPrint(allocator, "{s}", .{entry.key_ptr.*});
+            for (entry.value_ptr.array.items) |value| {
+                // std.debug.print("\t{s}\n", .{value.string});
+                // _ = home_path;
+                const result = try std.fmt.allocPrint(allocator, "{s}{s}", .{ self.home_path, value.string[1..] });
+                try self.add(category, result);
+            }
+        }
+        return self;
+    }
+
+    fn add(self: *IgnoreList, category: []const u8, path: []u8) !void {
+        std.debug.print("{} {s}\n", .{ &category, category });
+        var entry = try self.data.getOrPutValue(category, std.ArrayList([]u8).init(self.allocator));
+        return entry.value_ptr.append(path);
+    }
+
+    fn print(self: IgnoreList, writer: anytype) !void {
+        var iterator = self.data.iterator();
+        while (iterator.next()) |entry| {
+            try writer.print("{s}:\n", .{entry.key_ptr.*});
+            for (entry.value_ptr.items) |value| {
+                try writer.print("\t{s}\n", .{value});
+            }
+        }
+    }
+
+    pub fn jsonStringify(self: IgnoreList, jws: anytype) !void {
+        try jws.beginObject();
+        var iterator = self.data.iterator();
+        while (iterator.next()) |entry| {
+            // try jws.beginObject();
+            try jws.objectField(entry.key_ptr.*);
+            // try jw.beginArray();
+            // try jws.write(123);
+            // try jws.objectField(entry.key_ptr.*);
+            // try jws.write("a");
+            // try writer.print("{s}:\n", .{entry.key_ptr.*});
+            // try jws.valueStart();
+            try jws.beginArray();
+            for (entry.value_ptr.items) |value| {
+                try jws.print("\"~{s}\"", .{value[self.home_path.len..]});
+            }
+            try jws.endArray();
+            // try jws.valueEnd();
+            // try jws.endObject();
+        }
+        // try jws.objectField(self.);
+        // _ = self;
+        // var it = self.map.iterator();
+        // while (it.next()) |kv| {
+        //     try jws.objectField(kv.key_ptr.*);
+        //     try jws.write(kv.value_ptr.*);
+        // }
+        try jws.endObject();
+    }
+
+    fn commit_changes(self: IgnoreList) !void {
+        const file = try std.fs.cwd().createFile("ignore.json", .{});
+        defer file.close();
+        try std.json.stringify(self, .{ .whitespace = .indent_2 }, file.writer());
+    }
+
+    fn is_category_exist(self: IgnoreList, string: []const u8) ?[]const u8 {
+        var iterator = self.data.iterator();
+        while (iterator.next()) |entry| {
+            if (std.mem.eql(u8, entry.key_ptr.*, string)) {
+                return entry.key_ptr.*;
+            }
+        }
+        return null;
+    }
+
+    fn deinit(self: *IgnoreList) void {
+        var iterator = self.data.iterator();
+        while (iterator.next()) |entry| {
+            for (entry.value_ptr.items) |item| {
+                self.allocator.free(item);
+            }
+            self.allocator.free(entry.key_ptr.*);
+            entry.value_ptr.deinit();
+        }
+        self.data.deinit();
+    }
+
+    /// You need deinit ArrayList it by youself
+    fn get_flat_ignore_list(self: IgnoreList, allocator: std.mem.Allocator) !std.ArrayList([]u8) {
+        var result = std.ArrayList([]u8).init(allocator);
+        var iterator = self.data.iterator();
+        while (iterator.next()) |entry| {
+            for (entry.value_ptr.items) |value| {
+                try result.append(value);
+            }
+        }
+        return result;
+    }
+};
